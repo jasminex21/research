7c496ebe41d20fdc1faec2abd9816edec673c6e1
jasminex21 <jasminexu7003@icloud.com>
2024-09-07 00:19:30 -0500

    added relegated clubs to the dataset; should remove leicester, ipswich, southampton but keeping for now in case

--- data_scrape/tweetretriever.py
+++ data_scrape/tweetretriever.py
@@ -14,9 +14,9 @@ class Tweetretriever:
                  save_filepath="TWEETS.csv"):
 
         self.api = API()
-        self.club_handles = club_info["handle"]
-        self.ids = club_info["id"]
-        self.list_ids = club_info["list_id"]
+        self.club_handles = list(club_info["handle"])
+        self.ids = list(club_info["id"])
+        self.list_ids = list(club_info["list_id"])
         self.start_date = start_date
         self.end_date = end_date
         self.save_filepath = save_filepath
@@ -50,12 +50,15 @@ class Tweetretriever:
         for club_handle, list_id in zip(self.club_handles, self.list_ids): 
 
             club_tweets = await self.get_club_tweets(handle=club_handle, list_id=list_id)
-            time.sleep(180)
+            time.sleep(150)
             # remove tweets that are not by the club account
             club_tweets = club_tweets[club_tweets["club_handle"].str.lower() == club_handle]
             # add to all clubs df
             all_club_tweets = pd.concat([all_club_tweets, club_tweets])
             print(f"{club_tweets.shape[0]} tweets added for {club_handle}")
+        
+        all_club_tweets.to_csv(self.save_filepath, index=False)
+        print(f"Tweets for {self.club_handles} saved to {self.save_filepath}")
 
     async def get_tweet_reply(self):
         # GET USER GEOLOCATION; either tweet.place or tweet.user.location
@@ -65,9 +68,9 @@ class Tweetretriever:
         pass
 
 if __name__ == "__main__":
-    club_info = pd.read_csv("ACCOUNTS.csv")
-    retreiver = Tweetretriever(club_info=club_info,
+    club_info = pd.read_csv("/home/jasmine/PROJECTS/research/data_scrape/ACCOUNTS.csv")
+    retriever = Tweetretriever(club_info=club_info,
                                start_date="2023-08-11",
                                end_date="2024-05-19",
-                               save_filepath="TESTING.csv")
-    retreiver
+                               save_filepath="PARENT_TWEETS.csv")
+    asyncio.run(retriever.save_all_club_tweets())
